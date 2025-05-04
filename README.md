3f6a4089f8ab77541182aaebbe96d3413b6424ce
lavicardosoo <technisy@gmail.com>
2025-05-04 13:57:54 -0300

    3° commit trans

---
+++ README.md
@@ -0,0 +1,60 @@
+# 🧩 Monte o Raspberry Pi 4 – Jogo Educativo em Python
+
+Um mini-jogo educacional feito em **Python com Pygame**, onde o desafio é **montar corretamente os componentes de um Raspberry Pi 4** arrastando as peças para os lugares certos na placa.
+
+Ideal para quem está começando no mundo da computação física ou quer aprender sobre o hardware do Raspberry Pi de forma divertida!
+
+---
+
+## 🎮 Como jogar
+
+- Arraste e solte os componentes para os locais corretos da placa Raspberry Pi.
+- Cada parte está identificada com seu nome (CPU, RAM, USBs, etc.).
+- Clique na tecla 6 para sair do jogo <3.
+
+---
+
+## 🚀 Tecnologias utilizadas
+
+- [Python 3](https://www.python.org/)
+- [Pygame](https://www.pygame.org/)
+![pygame image](readme-media/pygame-icon.png)
+---
+
+## 📦 Instalação e execução
+
+1. Clone o repositório:
+   ```bash
+   git clone https://github.com/seu-usuario/raspberry-pi-builder.git
+   cd raspberry-pi-builder
+   ```
+
+2. Instale o Pygame:
+   ```bash
+   pip install pygame
+   ```
+
+3. Rode o jogo:
+   ```bash
+   python main.py
+   ```
+
+---
+
+## 🎯 Objetivo educacional
+
+Este jogo foi feito com o propósito de **ensinar de forma lúdica** os componentes que fazem parte do Raspberry Pi 4, reforçando o aprendizado visual e interativo. Ótimo para aulas de introdução à computação, robótica ou eletrônica.
+(Inclusive me ajudou bastante, pq tenho a memória de uma senhora de 80 anos🤧)
+
+---
+
+## 📸 Screenshot
+
+![Screenshot do jogo]("readme-media/captura1.png")
+![Screenshot do jogo]("readme-media/captura2.png")
+
+---
+
+## 📝
+O jogo pode ter alguns bugs cis.🫠
+Este projeto é de uso **educacional e livre**, você pode usar e adaptar como quiser. Mas lembre-se de dar os créditos! 😊
--- main.py
+++ main.py
@@ -7,7 +7,6 @@ def load(type,sound,img,width, height):
 	if type == "sound":
 		s = pg.mixer.Sound(sound)
 		return s
-		
 	elif type == "img":
 		i = pg.image.load(img)
 		i = pg.transform.scale(i,(width,height))
@@ -15,11 +14,16 @@ def load(type,sound,img,width, height):
 	
 pg.init()
 
+#define o tamanho da tela cis
 width = 950
 height = 700
+
+#ajusta a tela de acordo com as medidas passadas
 screen = pg.display.set_mode((width, height))
 
-#posição dos componentes 
+#posição origem e tamanho dos componentes 
+#p - se refere ao local certo onde as peças devem ficar
+#t - se referem ao tamanho do rect dessas peças
 p_cpu = (294, 298)
 t_cpu = (130,100)
 p_ram =  (431, 296)
@@ -37,30 +41,25 @@ t_ethernet = (170,100)
 p_w_b = (177, 242)
 t_w_b = (93,85)
 
-#imagens e áudios
+#carregando imagens e áudios
 bg = load("img", False, "media/bg.png", width, height)
 
 tittle = load("img", False, "media/text.png", 300,100)
 arrasou = load("img", False, "media/arrasou.png", 300,100)
 burra = load("img", False, "media/burra.png", 300,100)
-lucky = load("img", False, "media/img.png", 300,120)
+good_lucky = load("img", False, "media/img.png", 300,120)
 board_img = load("img",False, "media/board.png", 800,600)
 
 join_audio = pg.mixer.Sound("media/put.mp3")
-
-join_audio.set_volume(1.0)
-
-isso_bb = pg.mixer.Sound("media/aiqdlc.mp3")
-
+aiqdlc = pg.mixer.Sound("media/aiqdlc.mp3")
 inutil = pg.mixer.Sound("media/inutil.mp3")
 aiqvergonha = pg.mixer.Sound("media/aiqvergonha.mp3")
+aplausos = pg.mixer.Sound("media/aplausos.mp3")
+
+join_audio.set_volume(1.0)
 inutil.set_volume(1.0)
-
-isso_bb.set_volume(1.0)
-
-aê = pg.mixer.Sound("media/aplausos.mp3")
-
-aê.set_volume(0.7)
+aiqdlc.set_volume(1.0)
+aplausos.set_volume(0.7)
 
 components_img = {"cpu" : load("img", False, "media/cpu.png", 130, 100), 
 "ram" : load("img", False, "media/ram.png", 100,100), 
@@ -72,8 +71,7 @@ components_img = {"cpu" : load("img", False, "media/cpu.png", 130, 100),
 "ethernet" : load("img", False, "media/ethernet.png", 170,100)
 }
 
-#contador
-ok = 0
+
 
 #função para carregar imagens e áudios.
 
@@ -81,21 +79,14 @@ def load(type,sound,img,width, height):
 
 	if type == "sound":
 		s = pg.mixer.Sound(sound)
-
 		return s
-
-		
-
 	elif type == "img":
-
 		i = pg.image.load(img)
-
 		i = pg.transform.scale(i,(width,height))
-
 		return i		
 
 		
-#classes
+#classe a placa mãe
 class Board(pg.sprite.Sprite):
 	
 	def __init__(self):
@@ -105,35 +96,36 @@ class Board(pg.sprite.Sprite):
 		self.rect[0] = width / 2 - 400
 		self.rect[1] = 80
 
-#classe que irá dar vida aos objetos componentes 
+#classe que irá dar vida aos objetos dos componentes 
 class Components(pg.sprite.Sprite):
 	
-	#argumentos imagem, esquerda, direita, tamanho do rect que vai ser utilizado para definir o local do componente e a sua posição. 
+	#argumentos:imagem, esquerda, direita, tamanho do rect que vai ser utilizado para definir o local do componente e a sua posição. 
 	def __init__(self, img, left,top,rw_rh, rl_rr):
 		
 		pg.sprite.Sprite.__init__(self)
+
 		self.myplace = pg.Rect(rl_rr,(rw_rh[0] / 2,rw_rh[1] / 2)) 
 		self.myplace.top += 20
 		self.myplace.left += 30
+		
 		self.pos = rl_rr
 		self.image = img
+
 		self.rect = self.image.get_rect()
 		self.rect[0] = left
 		self.rect[1] = top
-		self.touched = False
 		self.rect[2], self.rect[3] = rw_rh
+
+		self.touched = False
 		self.joined = False
+
 		self.mouse = 0
 		self.state = [1,2,3]
 		self.state[0] = "parado"
 		
 	def update(self):
-		print(pg.mouse.get_pos())
-
-		#print(emojize("Monte esse rasberry pi trans :mouth:"))
 		if self.touched and self.joined == False:
-			#se o touched for verdadeiro o obj vai se mover de acordo com o movimento do cursor do mouse/dedo
-			#self.rect.move_ip(pg.mouse.get_pos())
+			#se o touched for verdadeiro o obj vai se mover de acordo com o movimento do cursor do mouse
 			self.rect[0],self.rect[1] = pg.mouse.get_pos()
 			self.state[0] = "em movimento"
 	
@@ -145,58 +137,48 @@ class Components(pg.sprite.Sprite):
 		
 
 	#testa se o componente está na sua posição de origem e o fixa		
-	def join(self,img,array):
+	def join(self,array):
 		if self.state[0] == "em movimento":
 			self.state[1] = "solto"
-		my_audio_choice = choice((join_audio,isso_bb))
+
+		my_audio_choice = choice((join_audio,aiqdlc))
 		my_badaudio_choice = choice((inutil,aiqvergonha))
+
 		if self.rect.colliderect(self.myplace) and self.joined == False:
 			self.state[1] = "no lugar de origem"
-			my_audio_choice.play()
-			img = arrasou
-			join_audio.play()
+			
 			self.rect[0], self.rect[1] = self.pos
 			self.joined = True
 			array.remove(self)
+			join_audio.play()
+			my_audio_choice.play()
 					
 		if self.state[1] == "solto" and self.joined == False:
 			img = burra
 			my_badaudio_choice.play()
 			self.state = [1,2,3]
-			
-	def notjoin(self):
-		pass
-
 
 #criando os objetos
-
 MotherBoard = Board()
-
 cpu = Components(components_img["cpu"], 800,400, t_cpu, p_cpu)
-
 ram = Components(components_img["ram"], 10,220, t_ram, p_ram)
-
 fonte = Components(components_img["fonte"],10,400,(t_fonte[0] + 30,t_fonte[1] + 10), p_fonte)
-
 usb2 = Components(components_img["usb2"], 100,100, t_usb2, p_usb2)
-
 usb3 = Components(components_img["usb3"], 200,600, t_usb3, p_usb3)
-
 w_b = Components(components_img["w&b"], 750,600, t_w_b, p_w_b)
-
 audio = Components(components_img["audio"], 100,600, t_audio, p_audio)
-
 ethernet = Components(components_img["ethernet"], 380,590, t_ethernet, p_ethernet)
 
 #grupo de sprites
 group = pg.sprite.Group()
 group.add(MotherBoard, cpu, ram, fonte, usb2, usb3, w_b, ethernet, audio)	
 
-#for tests
-myfont = pg.font.SysFont("font.ttf", 54)
-blit = False
-label = myfont.render(emojize("Monte esse rasberry pi trans :mouth:"), 1, (0,0,0))
+#lista que armazena os objetos dos componentes
 joined = [cpu,ram,fonte,usb2,usb3,w_b,audio,ethernet]
+
+#contador
+ok = 0
+
 while True:
 	
 	for ev in pg.event.get():
@@ -215,6 +197,7 @@ while True:
 			ethernet.touch(ev)
 
 		elif ev.type == pg.KEYDOWN:
+			#se clicar na tecla 6 o jogo fecha
 			if ev.key == 1073741918:
 				pg.quit()
 			
@@ -228,33 +211,31 @@ while True:
 			w_b.touched = False
 			audio.touched = False
 			ethernet.touched = False
-			cpu.join(lucky,joined)
-			ram.join(lucky,joined)
-			fonte.join(lucky,joined)
-			usb2.join(lucky,joined)
-			usb3.join(lucky,joined)
-			w_b.join(lucky,joined)
-			audio.join(lucky,joined)
-			ethernet.join(lucky,joined)
+
+			cpu.join(joined)
+			ram.join(joined)
+			fonte.join(joined)
+			usb2.join(joined)
+			usb3.join(joined)
+			w_b.join(joined)
+			audio.join(joined)
+			ethernet.join(joined)
 
 			print(joinedlen)
 			print(len(joined))
 
-			lucky = burra
+			good_lucky = burra
+
 			if joinedlen != len(joined):
-				lucky = arrasou
-			
-			blit == True
-	
-	#se todas as peças estiverem no seu devido local e o contador ser menor que 1 o áudio de aplausos será ativado e o cont irá receber +1.		
+				good_lucky = arrasou
+				
 	if cpu.joined and ram.joined and usb2.joined and usb3.joined and fonte.joined and ethernet.joined and audio. joined and w_b.joined and ok < 1:
-		aê.play()
+		aplausos.play()
 		ok+= 1
 							
 	screen.blit(bg, (0,0))
 	screen.blit(tittle,(350,20))
-	screen.blit(lucky,(350,90))
-		
+	screen.blit(good_lucky,(350,90))
 	group.draw(screen)
 	
 	cpu.update()
